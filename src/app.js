7265a4431c4969e78de8fe205fc394a52a8e3bd1
TomTheToad <tomthetoad951@protonmail.com>
2021-07-06 20:27:28 -0400

    Added prettier as dev dependency and .prettierrc for config

--- src/app.js
+++ src/app.js
@@ -3,16 +3,28 @@ const Pet = (props) => {
     React.createElement("h2", {}, props.name),
     React.createElement("h3", {}, props.animal),
     React.createElement("h3", {}, props.breed),
-  ])
-}
+  ]);
+};
 
 const App = () => {
   return React.createElement("div", {}, [
     React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, { name: "Kenai", animal: "Dog", breed: "Alaskan Malamute" }),
-    React.createElement(Pet, { name: "Nanook", animal: "Dog", breed: "Alaskan Malamute" }),
-    React.createElement(Pet, { name: "Mya", animal: "Dog", breed: "Alaskan Malamute" }),
+    React.createElement(Pet, {
+      name: "Kenai",
+      animal: "Dog",
+      breed: "Alaskan Malamute",
+    }),
+    React.createElement(Pet, {
+      name: "Nanook",
+      animal: "Dog",
+      breed: "Alaskan Malamute",
+    }),
+    React.createElement(Pet, {
+      name: "Mya",
+      animal: "Dog",
+      breed: "Alaskan Malamute",
+    }),
   ]);
 };
 
-ReactDOM.render(React.createElement(App), document.getElementById('root'));
+ReactDOM.render(React.createElement(App), document.getElementById("root"));
