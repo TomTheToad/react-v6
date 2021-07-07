5a2a80fad5a7d16ed5abd9f1521f16e51e700a35
TomTheToad <tomthetoad951@protonmail.com>
2021-07-06 20:04:46 -0400

    Added App.js, relocated js to App.js, created Pet component

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const Pet = (props) => {
+  return React.createElement("div", {}, [
+    React.createElement("h2", {}, props.name),
+    React.createElement("h3", {}, props.animal),
+    React.createElement("h3", {}, props.breed),
+  ])
+}
+
+const App = () => {
+  return React.createElement("div", {}, [
+    React.createElement("h1", {}, "Adopt Me!"),
+    React.createElement(Pet, { name: "Kenai", animal: "Dog", breed: "Alaskan Malamute" }),
+    React.createElement(Pet, { name: "Nanook", animal: "Dog", breed: "Alaskan Malamute" }),
+    React.createElement(Pet, { name: "Mya", animal: "Dog", breed: "Alaskan Malamute" }),
+  ]);
+};
+
+ReactDOM.render(React.createElement(App), document.getElementById('root'));
