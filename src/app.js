f0052ce0e2a3e9327ed799716eb4b517513a24c0
TomTheToad <tomthetoad951@protonmail.com>
2021-07-18 09:21:49 -0400

    Changed name of app.js to App.js. Updated App.js to jsx

--- src/app.js
+++ src/app.js
@@ -3,24 +3,14 @@ import ReactDOM from "react-dom";
 import Pet from "./Pet";
 
 const App = () => {
-  return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Kenai",
-      animal: "Dog",
-      breed: "Alaskan Malamute",
-    }),
-    React.createElement(Pet, {
-      name: "Nanook",
-      animal: "Dog",
-      breed: "Alaskan Malamute",
-    }),
-    React.createElement(Pet, {
-      name: "Mya",
-      animal: "Dog",
-      breed: "Alaskan Malamute",
-    }),
-  ]);
+  return (
+    <div>
+      <h1>Adopt Me!</h1>
+      <Pet name="Kenai" animal="Dog" breed="Alaskan Malamute" />
+      <Pet name="Nanook" animal="Dog" breed="Alaskan Malamute" />
+      <Pet name="Mya" animal="Dog" breed="Alaskan Malamute" />
+    </div>
+  )
 };
 
 ReactDOM.render(React.createElement(App), document.getElementById("root"));
