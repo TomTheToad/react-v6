b2c39b0291e0eef29a2c402232ad13b45ccd2bc7
TomTheToad <tomthetoad951@protonmail.com>
2021-07-18 08:45:13 -0400

    Installed and configured babel dependency.

--- src/app.js
+++ src/app.js
@@ -1,9 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet"
-
-
-
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
