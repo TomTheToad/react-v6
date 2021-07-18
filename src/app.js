db7d3aca4eda64667f5a31241bf0c815adc6217d
TomTheToad <tomthetoad951@protonmail.com>
2021-07-18 16:09:05 -0400

    Added SearchParam.js with SearchPara component for location search capability

--- src/app.js
+++ src/app.js
@@ -2,15 +2,17 @@
 // Keeping import for consistency but will remove from other components
 import React from "react";
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+// import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <Pet name="Kenai" animal="Dog" breed="Alaskan Malamute" />
+      <SearchParams />
+      {/* <Pet name="Kenai" animal="Dog" breed="Alaskan Malamute" />
       <Pet name="Nanook" animal="Dog" breed="Alaskan Malamute" />
-      <Pet name="Mya" animal="Dog" breed="Alaskan Malamute" />
+      <Pet name="Mya" animal="Dog" breed="Alaskan Malamute" /> */}
     </div>
   )
 };
