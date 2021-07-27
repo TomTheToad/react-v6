e3deb223be5373508e7ca3b1e2004fa3e51131d2
TomTheToad <tomthetoad951@protonmail.com>
2021-07-26 21:06:12 -0400

    Added ANIMALS const to SearchParams.js for selectable animal drop down

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,14 +1,35 @@
 import React from "react";
 import { useState } from "react";
 
+const ANIMALS =  ["bird", "cat", "dog", "rabbit", "reptile"];
+
 const SearchParams = () => {
   const [location, setLocation] = useState("Seattle, WA");
+  const [animal, setAnimal] = useState("");
+
   return (
     <div className="search-params">
       <form>
         <label htmlFor="location">
           Location
           <input id="location" onChange={(e) => setLocation(e.target.value)} value={location} placeholder="Location" />
+        </label>
+        <label htmlFor="animal">
+          Animal
+          <select id="animal"
+          value={animal} 
+          onChange = {e => setAnimal(e.target.value)}
+          onBlur = {e => setAnimal(e.target.value)}
+          >
+            <option value=""></option>
+            {
+              ANIMALS.map(animal => (
+                <option value={animal} key={animal}>{animal}</option>
+              ))
+            }
+
+          </select>
+
         </label>
         <button>Submit</button>
       </form>
