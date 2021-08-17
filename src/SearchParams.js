15cec59c2393aeed1f30f9e8b4bdf1f0de83689c
TomTheToad <tomthetoad951@protonmail.com>
2021-08-17 19:58:51 -0400

    Added Results component and replaced rnow redundant code in SearchParams.

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import Pet from "./Pet";
+import Results from "./Results";
 
 const ANIMALS = ["bird", "cat", "dog", "rabbit", "reptile"];
 
@@ -70,14 +70,7 @@ const SearchParams = () => {
         </label>
         <button>Submit</button>
       </form>
-      {pets.map((pet) => (
-        <Pet
-          name={pet.name}
-          animal={pet.animal}
-          breed={pet.breed}
-          key={pet.id}
-        />
-      ))}
+      <Results pets={pets} />
     </div>
   );
 };
